8de7702f673bf04c19091d06ecd18b57a7b27ca1
Okan Berber <okan.berber@outlook.com.tr>
2025-03-31 21:02:02 +0300

    Project Update

---
+++ HavamBatsinApp/HavamBatsinApp/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace HavamBatsinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_sicaklik = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_durum = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.lbl_sehir = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_sicaklik
+            // 
+            this.lbl_sicaklik.AutoSize = true;
+            this.lbl_sicaklik.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_sicaklik.ForeColor = System.Drawing.Color.White;
+            this.lbl_sicaklik.Location = new System.Drawing.Point(860, 273);
+            this.lbl_sicaklik.Name = "lbl_sicaklik";
+            this.lbl_sicaklik.Size = new System.Drawing.Size(370, 153);
+            this.lbl_sicaklik.TabIndex = 0;
+            this.lbl_sicaklik.Text = "00°C";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(619, 195);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(253, 221);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_durum
+            // 
+            this.lbl_durum.AutoSize = true;
+            this.lbl_durum.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_durum.ForeColor = System.Drawing.Color.White;
+            this.lbl_durum.Location = new System.Drawing.Point(593, 9);
+            this.lbl_durum.Name = "lbl_durum";
+            this.lbl_durum.Size = new System.Drawing.Size(447, 153);
+            this.lbl_durum.TabIndex = 4;
+            this.lbl_durum.Text = "durum";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Adana",
+            "Adıyaman",
+            "Afyonkarahisar",
+            "Ağrı",
+            "Aksaray",
+            "Amasya",
+            "Ankara",
+            "Antalya",
+            "Ardahan",
+            "Artvin",
+            "Aydın",
+            "Balıkesir",
+            "Bartın",
+            "Batman",
+            "Bayburt",
+            "Bilecik",
+            "Bingöl",
+            "Bitlis",
+            "Bolu",
+            "Burdur",
+            "Bursa",
+            "Çanakkale",
+            "Çankırı",
+            "Çorum",
+            "Denizli",
+            "Diyarbakır",
+            "Düzce",
+            "Edirne",
+            "Elazığ",
+            "Erzincan",
+            "Erzurum",
+            "Eskişehir",
+            "Gaziantep",
+            "Giresun",
+            "Gümüşhane",
+            "Hakkâri",
+            "Hatay",
+            "Iğdır",
+            "Isparta",
+            "İstanbul",
+            "İzmir",
+            "Kahramanmaraş",
+            "Karabük",
+            "Karaman",
+            "Kars",
+            "Kastamonu",
+            "Kayseri",
+            "Kırıkkale",
+            "Kırklareli",
+            "Kırşehir",
+            "Kilis",
+            "Konya",
+            "Kocaeli",
+            "Kütahya",
+            "Malatya",
+            "Manisa",
+            "Mardin",
+            "Mersin",
+            "Muğla",
+            "Muş",
+            "Nevşehir",
+            "Niğde",
+            "Ordu",
+            "Osmaniye",
+            "Rize",
+            "Sakarya",
+            "Samsun",
+            "Şanlıurfa",
+            "Siirt",
+            "Sinop",
+            "Sivas",
+            "Şırnak",
+            "Tekirdağ",
+            "Tokat",
+            "Trabzon",
+            "Tunceli",
+            "Uşak",
+            "Van",
+            "Yalova",
+            "Zonguldak"});
+            this.comboBox1.Location = new System.Drawing.Point(71, 367);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 39);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // lbl_sehir
+            // 
+            this.lbl_sehir.AutoSize = true;
+            this.lbl_sehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 100F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_sehir.ForeColor = System.Drawing.Color.White;
+            this.lbl_sehir.Location = new System.Drawing.Point(45, 146);
+            this.lbl_sehir.Name = "lbl_sehir";
+            this.lbl_sehir.Size = new System.Drawing.Size(379, 153);
+            this.lbl_sehir.TabIndex = 6;
+            this.lbl_sehir.Text = "Şehir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(1253, 595);
+            this.Controls.Add(this.lbl_sehir);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lbl_durum);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbl_sicaklik);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_sicaklik;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_durum;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label lbl_sehir;
+    }
+}
+
---
+++ HavamBatsinApp/HavamBatsinApp/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace HavamBatsinApp
+{
+    public partial class Form1 : Form
+    {
+        string seciliSehir = "Adana";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedItem = "Adana";
+            string path = "https://api.openweathermap.org/data/2.5/weather?q=" + seciliSehir + "&appid=68cc50411677424c33ee75803590a790&mode=xml&lang=tr&units=metric";
+            XDocument havadurumu = XDocument.Load(path);
+            XElement current = havadurumu.Root;
+
+            double sicaklik = Convert.ToInt32(Convert.ToDouble(current.Element("temperature").Attribute("value").Value.Replace('.', ',')));
+            lbl_sicaklik.Text = sicaklik.ToString() + "°C";
+
+            string durum = current.Element("weather").Attribute("value").Value;
+            string ikon = current.Element("weather").Attribute("icon").Value;
+            string imagePath = "https://openweathermap.org/img/wn/" + ikon + "@2x.png";
+            pictureBox1.ImageLocation = imagePath;
+            lbl_durum.Text = durum;
+            lbl_sehir.Text = seciliSehir;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            seciliSehir = comboBox1.SelectedItem.ToString();
+            SehirSec();
+        }
+        private void SehirSec()
+        {
+            string path = "https://api.openweathermap.org/data/2.5/weather?q=" + seciliSehir + "&appid=68cc50411677424c33ee75803590a790&mode=xml&lang=tr&units=metric";
+            XDocument havadurumu = XDocument.Load(path);
+            XElement current = havadurumu.Root;
+
+            double sicaklik = Convert.ToInt32(Convert.ToDouble(current.Element("temperature").Attribute("value").Value.Replace('.', ',')));
+            lbl_sicaklik.Text = sicaklik.ToString() + "°C";
+
+            string durum = current.Element("weather").Attribute("value").Value;
+            string ikon = current.Element("weather").Attribute("icon").Value;
+            string imagePath = "https://openweathermap.org/img/wn/" + ikon + "@2x.png";
+            pictureBox1.ImageLocation = imagePath;
+            lbl_durum.Text = durum;
+            lbl_sehir.Text = seciliSehir;
+        }
+    }
+}
